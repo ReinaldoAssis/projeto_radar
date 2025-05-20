f6f31e7bc7fe3510d399ac88512884f2da38f5d8
Reinaldo Assis <randroi75@gmail.com>
2025-05-20 12:15:17 -0300

    feat: network configuration and add SNTP support in project files

--- src/main.c
+++ src/main.c
@@ -8,31 +8,47 @@ LOG_MODULE_REGISTER(main);
 #define SENSOR_THREAD_STACK_SIZE 1024
 #define DISPLAY_THREAD_STACK_SIZE 1024
 #define MAIN_THREAD_STACK_SIZE 1024
+#define NETWORK_THREAD_STACK_SIZE 1024
 
 #define SENSOR_THREAD_PRIORITY 3
 #define DISPLAY_THREAD_PRIORITY 4
 #define MAIN_THREAD_PRIORITY 5
+#define NETWORK_THREAD_PRIORITY 4
 
 K_THREAD_STACK_DEFINE(sensor_stack, SENSOR_THREAD_STACK_SIZE);
 K_THREAD_STACK_DEFINE(display_stack, DISPLAY_THREAD_STACK_SIZE);
 K_THREAD_STACK_DEFINE(main_stack, MAIN_THREAD_STACK_SIZE);
+K_THREAD_STACK_DEFINE(network_stack, NETWORK_THREAD_STACK_SIZE);
 
 struct k_thread sensor_thread_data;
 struct k_thread display_thread_data;
 struct k_thread main_thread_data;
+struct k_thread network_thread_data;
 
 void display_thread(void *arg1, void *arg2, void *arg3) {
     while (1) {
-        LOG_INF("Thread de Display executando...");
+        print_log("Thread de Display executando...");
         k_msleep(1000); // Simulação temporária
     }
 }
 
 void main_thread(void *arg1, void *arg2, void *arg3) {
     while (1) {
-        LOG_INF("Thread Principal executando...");
+        print_log("Thread Principal executando...");
         k_msleep(1000); // Simulação temporária
     }
+}
+
+void network_thread(void *arg1, void *arg2, void *arg3) {
+    while (1) {
+        print_log("Thread de Rede executando...");
+        k_msleep(1000); // Simulação temporária
+    }
+}
+
+void print_log(const char *message) {
+	LOG_INF("%s", message);
+	printk("%s\n", message);
 }
 
 void main(void) {
@@ -45,9 +61,12 @@ void main(void) {
     k_thread_create(&main_thread_data, main_stack, MAIN_THREAD_STACK_SIZE,
                     main_thread, NULL, NULL, NULL,
                     MAIN_THREAD_PRIORITY, 0, K_NO_WAIT);
+    k_thread_create(&network_thread_data, network_stack, NETWORK_THREAD_STACK_SIZE,
+                    network_thread, NULL, NULL, NULL,
+                    NETWORK_THREAD_PRIORITY, 0, K_NO_WAIT);
 
-    LOG_INF("Sistema inicializado!");
-	printk("Sistema inicializado!\n");
+	print_log("Sistema inicializado!");
+
 	while (1) {
 		k_msleep(1000); // Manter o loop principal ativo
 	}
